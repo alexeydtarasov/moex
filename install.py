b642aec8fc9a3f5f5b997cd17cafdcf189d24ea4
alexeydtarasov <alexey.d.tarasov@yandex.ru>
2021-06-03 23:41:08 +0300

    upd installation

--- install.py
+++ install.py
@@ -7,6 +7,7 @@ cur_path = os.getcwd()
 
 if 'linux' in sys.platform.lower() or 'mac' in sys.platform.lower():
     shell = subprocess.check_output('echo $SHELL', shell=True).decode().strip().split('/')[-1]
-    print(shell)
     os.system(f'echo "export PYTHONPATH=$PYTHONPATH:{cur_path}" >> ~/.{shell}rc')
+
+print('Installation finished!')
 
