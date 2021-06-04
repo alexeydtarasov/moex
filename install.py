f34cb972d064a1abfa08e7064700ea2b93b01450
alexeydtarasov <alexey.d.tarasov@yandex.ru>
2021-06-04 11:35:49 +0300

    sourcing *shrc

--- install.py
+++ install.py
@@ -7,7 +7,8 @@ cur_path = os.getcwd()
 
 if 'linux' in sys.platform.lower() or 'mac' in sys.platform.lower():
     shell = subprocess.check_output('echo $SHELL', shell=True).decode().strip().split('/')[-1]
-    os.system(f'echo "export PYTHONPATH=$PYTHONPATH:{cur_path}" >> ~/.{shell}rc')
+    command = f'echo "export PYTHONPATH=$PYTHONPATH:{cur_path}" >> ~/.{shell}rc && source ~/.{shell}rc'
+    os.system(command)
 
 print('Installation finished!')
 
