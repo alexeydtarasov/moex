c282e4066a5bfa3dda39f1f5427651f70978fd5b
alexeydtarasov <alexey.d.tarasov@yandex.ru>
2021-06-08 12:10:23 +0300

    gitignore

--- config.py
+++
@@ -1,8 +0,0 @@
-import logging
-
-LOG_LEVEL = logging.DEBUG
-
-logging.basicConfig(format="[%(levelname)s] %(asctime)s | %(message)s", datefmt='%Y-%m-%d %H:%M:%S', level=LOG_LEVEL)
-logger = logging.getLogger()
-
-MOEX_TOKEN_PATH = '/home/denis/PycharmProjects/common_data/moex_token.txt'
